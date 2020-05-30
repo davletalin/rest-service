d047a232de172e0de4c5d1a9919098668a28be96
Armat <davletalin@gmail.com>
2020-05-31 00:22:21 +0600

    Algorithm refactored using service

--- src/main/java/kz/davletalin/fibo/restservice/model/Fibonacci.java
+++ src/main/java/kz/davletalin/fibo/restservice/model/Fibonacci.java
@@ -5,33 +5,16 @@ import java.math.BigInteger;
 public class Fibonacci {
     private BigInteger fibonacciValue;
 
-    public Fibonacci(long fibonacciIndex) {
-        this.calculateFibo(fibonacciIndex);
+    public Fibonacci(BigInteger fibonacciValue) {
+        this.fibonacciValue = fibonacciValue;
     }
 
-    private final BigInteger calculateFibo(long fibonacciIndex) {
-        BigInteger a = BigInteger.valueOf(0L);
-        BigInteger b = BigInteger.valueOf(1L);
-        if (fibonacciIndex == 0L) {
-            return this.fibonacciValue = a;
-        } else {
-            for (int i = 2; (long) i <= fibonacciIndex; ++i) {
-                BigInteger c = a.add(b);
-                a = b;
-                b = c;
-            }
-
-            return this.fibonacciValue = b;
-        }
+    public BigInteger getFibonacciValue() {
+        return fibonacciValue;
     }
 
-
-    public BigInteger getFibonacci() {
-        return this.fibonacciValue;
-    }
-
-    public void setFibonacci(BigInteger fibonacci) {
-        this.fibonacciValue = fibonacci;
+    public void setFibonacciValue(BigInteger fibonacciValue) {
+        this.fibonacciValue = fibonacciValue;
     }
 }
 
--- src/main/java/kz/davletalin/fibo/restservice/service/FibonacciServiceImpl.java
+++ src/main/java/kz/davletalin/fibo/restservice/service/FibonacciServiceImpl.java
@@ -1,10 +1,29 @@
 package kz.davletalin.fibo.restservice.service;
 
 import kz.davletalin.fibo.restservice.model.Fibonacci;
+import org.springframework.stereotype.Service;
 
+import java.math.BigInteger;
+
+@Service
 public class FibonacciServiceImpl implements FibonacciService{
     @Override
     public Fibonacci get(long fibonacciIndex) {
-        return null;
+        BigInteger fibonacciValue = calculateFibo(fibonacciIndex);
+        return new Fibonacci(fibonacciValue);
+    }
+    private final BigInteger calculateFibo(long fibonacciIndex) {
+        BigInteger a = BigInteger.valueOf(0L);
+        BigInteger b = BigInteger.valueOf(1L);
+        if (fibonacciIndex == 0L) {
+            return a;
+        } else {
+            for (int i = 2; (long) i <= fibonacciIndex; ++i) {
+                BigInteger c = a.add(b);
+                a = b;
+                b = c;
+            }
+            return b;
+        }
     }
 }
